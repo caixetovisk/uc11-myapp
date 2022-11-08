96daf4d33e65d139503b3050a0ac1d84526685f1
GABRIEL CAIXETA SILVA <gabriel.silva@prof.sc.senac.br>
2022-11-08 17:05:00 -0300

    aula 08/11/2022

--- src/composable/userPhotoGallery.ts
+++ src/composable/userPhotoGallery.ts
@@ -9,7 +9,15 @@ export interface UserPhoto {
 }
 
 export function userPhotoGallery() {
+  const PHOTO_STORAGE = 'photos';
   const photos = ref<UserPhoto[]>([]);
+
+  const cachePhotos = () => {
+    Preferences.set({
+      key: PHOTO_STORAGE,
+      value: JSON.stringify(photos.value), // JSON.parse(string) 
+    });
+  };
 
   const convertBlobToBase64 = (blob: Blob) =>
     new Promise((resolve, reject) => {
@@ -21,6 +29,22 @@ export function userPhotoGallery() {
       reader.readAsDataURL(blob);
     });
 
+    const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+      //let base64Data: string;
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+      const base64Data = (await convertBlobToBase64(blob)) as string;
+      const savedFile = await Filesystem.writeFile({
+        path: fileName,
+        data: base64Data,
+        directory: Directory.Data,
+      });
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath,
+      };
+    };
+
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
@@ -28,13 +52,28 @@ export function userPhotoGallery() {
       quality: 80,
     });
     const fileName = new Date().getTime() + '.jpeg';
-    const savedFileImage = {
-      filepath: fileName,
-      webviewPath: photo.webPath,
-    };
+    const savedFileImage = await savePicture(photo, fileName);
 
     photos.value = [savedFileImage, ...photos.value];
   };
+
+  const loadSaved = async () => {
+    const photoList = await Preferences.get({ key: PHOTO_STORAGE });
+    const photosInPreferences = photoList.value ? JSON.parse(photoList.value) : [];
+  
+    for (const photo of photosInPreferences) {
+      const file = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data,
+      });
+      photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+    }
+  
+    photos.value = photosInPreferences;
+  };
+
+  watch(photos, cachePhotos);
+  onMounted(loadSaved);
   return {
     photos,
     takePhoto,
